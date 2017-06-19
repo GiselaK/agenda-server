274b48f5de5d40783e6de7c92b8632f5cdd26fb5
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-06-19 10:20:50 -0700

    Removed temporary debug deployment server code, replaced with original mongoose query

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,9 @@ var router = express.Router();
 var user = require('../controllers/user')
 
 router.post('/newUser', function(req, res, next) {
-  // user.createUser("asas", function (id, status) {
-  //   res.json(id)
-  // })
-  res.json({id: 1});
+  user.createUser("asas", function (id, status) {
+    res.json(id)
+  })
 })
 
 module.exports = router;
