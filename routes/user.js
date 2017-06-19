55834b8aae46ce8c9626a2c28b0cccb9460cbe37
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-06-19 10:10:42 -0700

    TEMPORARY: Testing deployment server Checking if crashing due to database configuration

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ var router = express.Router();
 var user = require('../controllers/user')
 
 router.post('/newUser', function(req, res, next) {
-  user.createUser("asas", function (id, status) {
-    res.json(id)
-  })
+  // user.createUser("asas", function (id, status) {
+  //   res.json(id)
+  // })
+  res.json({id: 1});
 })
 
 module.exports = router;
