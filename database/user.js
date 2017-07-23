402165e3d7707220db5b546f5f089023529adbe2
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-07-22 19:46:00 -0700

    Switched expiration date to date format and changed indenting for clarity

--- database/user.js
+++ database/user.js
@@ -4,10 +4,24 @@ var ObjectId = Schema.ObjectId;
 
 var userSchema = new Schema({
   id: ObjectId,
-  google: {refresh_token: String, last_updated: Date, next_sync_token: String, access_token: {token: String, expiration_date: String }},
-  facebook: {refresh_token: String, last_updated: Date},
-  meetup: {refresh_token: String, last_updated: Date},
-  name: String
+  google: {
+    refresh_token: String,
+    last_updated: Date, 
+    next_sync_token: String,
+    name: String,
+    access_token: {
+      token: String, 
+      expiration_date: Date
+    }
+  },
+  facebook: {
+    refresh_token: String, 
+    last_updated: Date
+  },
+  meetup: {
+    refresh_token: String, 
+    last_updated: Date
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
