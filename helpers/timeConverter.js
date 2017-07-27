4137cf0c248c11762efd50635bcc6f8037da2f7b
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-07-26 18:16:37 -0700

    Testing: Add no offset to rftc3339 time

--- helpers/timeConverter.js
+++ helpers/timeConverter.js
@@ -9,7 +9,9 @@ exports.convertToRFC339 = function (date) {
     '-' + pad(d.getUTCDate()) +
     'T' + pad(d.getUTCHours()) +
     ':' + pad(d.getUTCMinutes()) 
-    ':' + pad(d.getUTCSeconds());
+    ':' + pad(d.getUTCSeconds()) + 
+    '+' + '00:00'; //Timezone offset:
+     //Since user is choosing time presumably in their timezone setting the offset to always be 0.
   }
   return ISODateString(date);
 };
