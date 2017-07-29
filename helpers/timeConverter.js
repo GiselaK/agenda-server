a3367f10267dcb24f4d0f72e02354627712698ed
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-07-28 18:17:18 -0700

    Removed timeset offset from rfc339 helper

--- helpers/timeConverter.js
+++ helpers/timeConverter.js
@@ -9,8 +9,8 @@ exports.convertToRFC339 = function (date) {
     '-' + pad(d.getUTCDate()) +
     'T' + pad(d.getUTCHours()) +
     ':' + pad(d.getUTCMinutes()) +
-    ':' + pad(d.getUTCSeconds()) + 
-    '+' + '00:00'; //Timezone offset:
+    ':' + pad(d.getUTCSeconds())
+    // '+' + '00:00'; //Timezone offset:
      //Since user is choosing time presumably in their timezone setting the offset to always be 0.
   }
   return ISODateString(date);
