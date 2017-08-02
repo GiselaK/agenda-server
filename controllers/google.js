f968bc9eff5eae9c57dbe3d15c9aeb9ff5f35fb1
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-08-02 16:48:34 -0700

    added log

--- controllers/google.js
+++ controllers/google.js
@@ -27,8 +27,9 @@ exports.getRefreshToken = function (userID, code, next) {
 };
 
 exports.retrieveAccessToken = function (userID, next) {
-  
+
   function getAccessToken (refreshToken) {
+    console.log("Recieved refresh token:", refreshToken)
     request.post({url: tokenURL, form: {refresh_token: refreshToken, client_id: googleAuth.client_id, client_secret: googleAuth.client_secret, grant_type: 'refresh_token'}}, function (err, resp, body) {
       if (!err) {
         var accessToken = JSON.parse(body).access_token;
