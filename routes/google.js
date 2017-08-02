b9f795fbc6e18516fa16b609c9b352f15d0c6b2b
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-08-01 17:05:17 -0700

    Fix send file

--- routes/google.js
+++ routes/google.js
@@ -4,7 +4,7 @@ var google = require('../controllers/google');
 var user = require('../controllers/user');
 
 router.get('/webhook', function (req, res, next) {
-  res.sendfile(__dirname + '/googleWebhook.html');
+  res.sendfile(__dirname + '../public/googleWebhook.html');
 })
 
 router.post('/oauth', function (req, res, next) {
