4ec532db1fd220b31e71e49d6358b0d83e2ce5dd
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-08-01 17:00:52 -0700

    Fix send file

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/meetup', meetup);
 app.use('/google', google);
 app.use('/me', user);
