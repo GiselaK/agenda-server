78e6f31a773f39ec6dc1a383fe088b68a2f46448
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-10-23 14:12:32 -0700

    Fixed referrer logic (by handeling in swift instead of the webpage so that only one webpage is needed for all webhooks)

--- routes/meetup.js
+++ routes/meetup.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var meetup = require('../controllers/meetup');
 
+router.get('/hook', function (req, res, next) {
+  res.sendFile(path.resolve(__dirname + '/../public/meetupWebhook.html'));
+});
+
 router.post('/oauth', function (req, res, next) {
   meetup.getRefreshToken(req.body.code, function () {
     res.send(200);
