7f5f3c71d20af0dde22607f441df5e4bced93e8a
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-11-06 15:12:15 -0800

    Added api routes documentation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # agenda-server
+
+Update Docs:
+apidoc -i routes/ .
--- routes/google.js
+++ routes/google.js
@@ -6,23 +6,81 @@ var path = require('path');
 
 router.get('/webhook', function (req, res, next) {
   res.sendFile(path.resolve(__dirname + '/../public/googleWebhook.html'));
-})
+});
+
+/**
+ * @api {GET} /oauth Get Users Refresh Token
+ * @apiVersion 0.1.0
+ * @apiName Authenticate
+ * @apiGroup Google
+ *   
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ */
 
 router.post('/oauth', function (req, res, next) {
-  console.log("da body is:",req.body);
   google.getRefreshToken(req.body.id, req.body.code, function (status) {
     res.sendStatus(status);
   });
 });
 
+/**
+ * @api {GET} /getCals/:userID Get Users Calendars
+ * @apiVersion 0.1.0
+ * @apiName Get Users Calendars
+ * @apiGroup Google
+ * 
+ * @apiParam {userID} Users Unique ID
+ * 
+ * @apiSuccess {Object[]} calendars List of all users Google Calendars
+ *  
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *    {
+ *      "calendars": [{
+ *        "id": "eji3i3n2",
+ *        "name": "abc@gmail.com",
+ *        "timeZone": ""
+ *    }]
+ *    }
+ */
+
 router.post('/getCals/:userID', function (req, res, next) {
   user.retrieveAccessToken(req.params.userID, function (err, accessToken) {
     google.getCals(accessToken, function (err, status, data) {
-        console.log(err ? "Error:" + err: data);
         res.json(data);
     });
   });
 });
+
+/**
+ * @api {GET} /getEvents/:calID/:userID/:nextPage? Get Events From Calendar
+ * @apiVersion 0.1.0
+ * @apiName Get Events
+ * @apiGroup Google
+ * 
+ * @apiParam {calID} Calendars Unique ID
+ * @apiParam {userID} Users Unique ID
+ * @apiParam {nextPage} Next Page Token Recieved From Previous Call To This Endpoint
+ * 
+ * @apiSuccess {Object[]} events 250 Event objects (unless last page then it will be however many are remaining)
+ * @apiSuccess {String} nextPage nextPageToken to redo request for more events or if it's the last page "undefined"
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "events": [{
+ *        "id": "",
+ *        "name": "Sample Event",
+ *        "description": "Details About This Event",
+ *        "src": "google",
+ *        "startTime": "",
+ *        "endTime": "",
+ *        "location": ""
+ *       },...],
+ *       "nextPage": "2jkd23jn2ndn2"
+ *     }
+ */
 
 router.post('/getEvents/:calID/:userID/:nextPage?', function (req, res, next) {
   user.retrieveAccessToken(req.params.userID, function (err, accessToken) {
@@ -32,13 +90,25 @@ router.post('/getEvents/:calID/:userID/:nextPage?', function (req, res, next) {
   });
 });
 
+/**
+ * @api {POST} /createEvent/:calID/:userID Add Event To Calendar
+ * @apiVersion 0.1.0
+ * @apiName Add Event
+ * @apiGroup Google
+ * 
+ * @apiParam {calID} Calendars Unique ID
+ * @apiParam {userID} Users Unique ID
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *
+ */
 router.post('/createEvent/:calID/:userID', function (req, res, next) {
   user.retrieveAccessToken(req.params.userID, function (err, accessToken) {
-    console.log("ABOuT TO CREATE EVENT  WITH TOKEN:", accessToken)
     google.createEvent(accessToken, req.body.event, req.params.calID, req.body.timeZone, function (status) {
       res.sendStatus(status);
     });
-  })
+  });
 });
 
 module.exports = router;
